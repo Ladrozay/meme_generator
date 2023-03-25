29ba02ba6203f24d73802138fdf851a202e8f8b8
Kolade Abdulrahman <abdulrahmankolade@gmail.com>
2023-03-25 09:36:49 -0700

    uploaded meme generator

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+export default function Footer() {
+    return (
+      <footer className="footer">
+        <small>
+          Made by{" "}
+          <a
+            className="footer--link"
+            href="https://emanueledelmonte.it"
+            target="_blank"
+            rel="noreferrer"
+          >
+           Kolade Abdulrahman
+          </a>
+          .
+        </small>
+      </footer>
+    );
+  }
+  
+  
